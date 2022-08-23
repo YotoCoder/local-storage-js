98ca2a7c41f79ea231f020231cd08256d13dd58c
Yonathan Soto <yotoelectronics@gmail.com>
2022-08-23 07:39:24 -0500

    first commit xd

---
+++ local.js
@@ -0,0 +1,35 @@
+class Local{
+	
+	constructor(key){
+		this.key = key
+		this.item = this.get()
+
+		if(this.item === null){
+			this.set([])
+		}
+	}
+
+	set(value){
+		localStorage.setItem(this.key, JSON.stringify(value))
+	}
+
+	get(){
+		return JSON.parse(localStorage.getItem(this.key))
+	}
+
+	add(value){
+		let data = this.get()
+		data.push(value)
+		this.set(data)
+	}
+
+	delete(id){
+		let list = this.get()
+	    for(var i = list.length - 1; i>=0 ;i--){
+	    	console.log(i)
+	        if(list[i].id == id)
+	            list.splice(i, 1);
+	    }
+	   	this.set(list)
+	}
+}
